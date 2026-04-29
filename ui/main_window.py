11cf42ef4c6175463ceb5bf3bf58def73cef4ee6
9hokage4 <claymamedov206@gmail.com>
2026-04-29 18:24:33 +0300

    Я сделал вкладку с настройками

--- ui/main_window.py
+++ ui/main_window.py
@@ -10,8 +10,7 @@ import pandas as pd
 from ui.components.nav_button import NavButton
 from ui.components.upload_widget import UploadWidget
 from ui.components.results_widget import ResultsWidget
-from ui.components.history_widget import HistoryWidget
-
+from ui.components.settings_widget import SettingsWidget  # исправлен импорт
 
 # Параметры навигационной панели
 NAV_PANEL_PARAMS = {
@@ -21,7 +20,6 @@ NAV_PANEL_PARAMS = {
     'sidebar_padding_right': 32,
     'btn_spacing': 20,
 }
-
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -41,23 +39,18 @@ class MainWindow(QMainWindow):
         nav_container = QWidget()
         nav_container.setFixedSize(NAV_PANEL_PARAMS['sidebar_width'], NAV_PANEL_PARAMS['sidebar_height'])
         nav_container.setObjectName("nav_container")
-
-        # Стиль панели: белый фон, скругленные углы
         nav_container.setStyleSheet("""
             #nav_container {
                 background-color: #FFFFFF;
                 border-radius: 44px;
             }
         """)
-
-        # Тень
         shadow = QGraphicsDropShadowEffect()
         shadow.setBlurRadius(30)
         shadow.setOffset(0, 6)
         shadow.setColor(QColor(0, 0, 0, 30))
         nav_container.setGraphicsEffect(shadow)
 
-        # Горизонтальный layout для кнопок
         nav_horizontal_layout = QHBoxLayout()
         nav_horizontal_layout.setContentsMargins(
             NAV_PANEL_PARAMS['sidebar_padding_left'],
@@ -73,8 +66,8 @@ class MainWindow(QMainWindow):
         self.nav_buttons = []
         btn_data = [
             ("resources/icons/upload.svg", "Загрузка"),
-            ("resources/icons/chart-line.svg", "Результаты"),
-            ("resources/icons/history.svg", "История")
+            ("resources/icons/cog.svg", "Настройки"),  # иконка настроек (замените при необходимости)
+            ("resources/icons/chart-line.svg", "Результаты")
         ]
         for icon, text in btn_data:
             btn = NavButton(icon, text)
@@ -95,31 +88,26 @@ class MainWindow(QMainWindow):
 
         # Создаём страницы
         self.upload_tab = UploadWidget()
+        self.settings_tab = SettingsWidget()
         self.results_tab = ResultsWidget()
-        self.history_tab = HistoryWidget()
-        self.history_tab.load_to_current_requested.connect(self.on_load_history_to_current)
 
-        # Настройка размера шрифта и иконок кнопок навигации
+        # Настройка результатов
         self.results_tab.set_nav_font_size(14)
         self.results_tab.set_nav_icon_size(40)
 
         self.stacked_widget.addWidget(self.upload_tab)
+        self.stacked_widget.addWidget(self.settings_tab)
         self.stacked_widget.addWidget(self.results_tab)
-        self.stacked_widget.addWidget(self.history_tab)
 
         # Подключаем сигналы
         self.upload_tab.file_loaded.connect(self.on_file_loaded)
-        self.upload_tab.settings_requested.connect(self.open_settings_dialog)
+        self.upload_tab.settings_requested.connect(self.open_settings_tab)  # теперь переключает на вкладку
         self.upload_tab.processing_finished.connect(self.on_processing_finished)
         self.upload_tab.data_cleared.connect(self.on_data_cleared)
 
-        print("Nav buttons created:", len(self.nav_buttons))
-        print("Nav container size:", nav_container.size())
-        print("Nav container layout:", nav_container.layout())
-        print("First button visible:", self.nav_buttons[0].isVisible())
+        self.settings_tab.config_saved.connect(self.on_config_saved)
 
         self.current_config = None
-        self.current_profile_name = "" 
 
     def switch_to_tab(self, button: NavButton, tab_name: str):
         """Переключает вкладку по нажатию на кнопку навигации."""
@@ -129,72 +117,32 @@ class MainWindow(QMainWindow):
 
         if tab_name == "Загрузка":
             self.stacked_widget.setCurrentWidget(self.upload_tab)
+        elif tab_name == "Настройки":
+            self.stacked_widget.setCurrentWidget(self.settings_tab)
         elif tab_name == "Результаты":
             self.stacked_widget.setCurrentWidget(self.results_tab)
-        elif tab_name == "История":
-            self.stacked_widget.setCurrentWidget(self.history_tab)
-            
-    def on_load_history_to_current(self, summary_data, charts_data):
-        """Загружает исторические данные в текущую сессию."""
-        # Переключаемся на вкладку результатов
-        self.switch_to_tab(self.nav_buttons[1], "Результаты")
-        # Передаём данные в results_tab (table_df не нужен, но для set_data требуется DataFrame)
-        empty_df = pd.DataFrame()
-        self.results_tab.set_data(empty_df, charts_data, summary_data)
+
+    def open_settings_tab(self):
+        """Переключает на вкладку Настройки."""
+        self.switch_to_tab(self.nav_buttons[1], "Настройки")
 
     def on_file_loaded(self, df):
-        """Обработчик загрузки файла."""
         pass
 
     def on_processing_finished(self, table_df, charts_data, summary_data, success, message):
         if success:
-            self.switch_to_tab(self.nav_buttons[1], "Результаты")
+            self.switch_to_tab(self.nav_buttons[2], "Результаты")
             self.results_tab.set_data(table_df, charts_data, summary_data)
-
-            from utils.history_manager import HistoryManager
-            hm = HistoryManager()
-            # Берём актуальное имя профиля
-            profile_name = getattr(self, 'current_profile_name', '')
-            if not profile_name:
-                profile_name = self.upload_tab.config_summary.profile_name
-            if profile_name == "Без профиля":
-                profile_name = ""
-            metadata = {
-                "original_filename": self.upload_tab.current_file_path.split("/")[-1] if self.upload_tab.current_file_path else "неизвестно",
-                "test_name": self.current_config.get("test_name", ""),
-                "profile_name": profile_name,
-                "respondents_count": len(table_df),
-                "groups_count": len(summary_data.get("group_summary", {})),
-                "courses_count": len(summary_data.get("course_summary", {}))
-            }
-            hm.add_record(metadata, summary_data, charts_data)
-            self.history_tab.load_history_list()
+        else:
+            # Обработка ошибки
+            pass
 
     def on_data_cleared(self):
-        """Обработчик очистки данных (при нажатии 'Отмена')."""
-        # Очищаем таблицу результатов
         self.results_tab.clear_data()
 
-    def open_settings_dialog(self):
-        from ui.components.settings_dialog import SettingsDialog
-        dialog = SettingsDialog(self, self.current_config)
-        dialog.config_saved.connect(self.on_config_saved)
-        dialog.profile_loaded_with_name.connect(self.on_profile_loaded)
-        dialog.exec()
-
-    def on_profile_loaded(self, profile_name, config):
-        """Обработчик загрузки профиля с именем."""
-        self.current_profile_name = profile_name   # <-- добавить
-        self.current_config = config
-        self.upload_tab.set_config(config)
-        self.upload_tab.config_summary.set_profile_name(profile_name)
-        self.upload_tab.config_summary.update_config_from_dict(config)
-
     def on_config_saved(self, full_config, profile_name):
-        """Обработчик сохранения конфигурации."""
-        # Сохраняем полную конфигурацию (всегда, даже без профиля)
+        """Сохраняет конфигурацию, полученную из SettingsWidget."""
         self.current_config = full_config
-        # Передаём в upload_widget
         self.upload_tab.set_config(full_config)
-        # Обновляем имя профиля
-        self.upload_tab.config_summary.set_profile_name(profile_name if profile_name else "Без профиля")
+        # Опционально: переключиться на вкладку Загрузка после сохранения
+        self.switch_to_tab(self.nav_buttons[0], "Загрузка")
